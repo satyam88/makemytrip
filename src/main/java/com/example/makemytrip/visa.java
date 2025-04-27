cf7072c289ef5c656de529c8dc0fbad0c4050c80
satyam88 <satyampandey88@gmail.com>
2025-04-27 08:46:11 +0530

    Update visa.java

--- src/main/java/com/example/makemytrip/visa.java
+++ src/main/java/com/example/makemytrip/visa.java
@@ -1,1 +1,10 @@
+package com.example.makemytrip;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class visa {
+    @GetMapping("/visa")
+    public String getData() {return  "Please sssss your hotels from mmt kindly book ticket for New Delhi to anywhere at 20% discount" ; }
+}
