0807506ceeb667270b2765cdb95a6ffe3a114f15
cloudanddevops88 <satyampandey88@gmail.com>
2025-04-27 08:08:24 +0530

    change in pom.xml

--- src/main/java/com/example/makemytrip/flights.java
+++ src/main/java/com/example/makemytrip/flights.java
@@ -6,5 +6,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class flights {
     @GetMapping("/flights")
-    public String getData() {return  "Please book your hotels from mmt kindly book ticket for New Delhi to anywhere at 20% discount" ; }
+    public String getData() {return  "Please sssss your hotels from mmt kindly book ticket for New Delhi to anywhere at 20% discount" ; }
 }
--- src/main/java/com/example/makemytrip/holidays.java
+++ src/main/java/com/example/makemytrip/holidays.java
@@ -6,5 +6,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class holidays {
     @GetMapping("/holidays")
-    public String getData() {return  "Please book your hotels from mmt kindly book ticket for New Delhi to anywhere at 20% discount" ; }
+    public String getData() {return  "sssss book your hotels from mmt kindly book ticket for New Delhi to anywhere at 20% discount" ; }
 }
