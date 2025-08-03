a0c626ecdc67cd016fcaf006d1e43f7e1c5452d4
satyam88 <satyampandey88@gmail.com>
2025-08-03 12:07:49 +0530

    Update train.java

--- src/main/java/com/example/makemytrip/train.java
+++ src/main/java/com/example/makemytrip/train.java
@@ -6,5 +6,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class train {
     @GetMapping("/train")
-    public String getData() {return  "Please dev-B dev-A book flights indigo ticket with 50% discount" ; }
+    public String getData() {return  "Please dev-B dev-A book flights indigo ticket with 1110% discount" ; }
 }
