388b8f632f19f125173ef270ac3ee73d83d29067
satyam88 <satyampandey88@gmail.com>
2025-08-16 12:21:10 +0530

    hello

--- src/main/java/com/example/makemytrip/holidays.java
+++ src/main/java/com/example/makemytrip/holidays.java
@@ -6,5 +6,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class holidays {
     @GetMapping("/holidays")
-    public String getData() {return  "Please  Dev-A book flights and holiday indigo ticket with 50% discount" ; }
+    public String getData() {return  "Please  Dev-A and Dve 888 book flights and holiday indigo ticket with 50% discount" ; }
 }
